5e65928130a6d8a685c123850393630b9c5d7b7f
Chukwuma Obasi <ccobasi8@gmail.com>
2021-06-02 23:20:57 +0100

    Add styling to home page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import Navigation from './nav';
 import initPage from './initial';
 import homePage from './home';
+import './main.css';
 
 function html() {
     const link = document.querySelector("link[rel*='icon']")
@@ -15,3 +17,29 @@ function html() {
   const mainContent = document.querySelector('.main-content');
   mainContent.append(homePage());
   mainContent.append(html());
+
+const nav = new Navigation();
+const pages = document.querySelectorAll('.pages');
+for (let i = 1; i < pages.length; i) {
+  pages[i].classList.add('hide');
+}
+
+const hidePages = () => {
+  for (let i = 0; i < pages.length; i) {
+    pages[i].classList.add('hide');
+  }
+};
+
+const ctaBtn = document.querySelector('.cta-btn');
+ctaBtn.addEventListener('click', () => {
+  const menu = document.querySelector('.menu-page');
+  hidePages();
+  menu.classList.remove('hide');
+});
+
+const logo = document.querySelector('.logo');
+logo.addEventListener('click', () => {
+  const home = document.querySelector('.home-page');
+  hidePages();
+  home.classList.remove('hide');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
@@ -15,6 +15,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
