5e6872e6a94c3bf342eb06327d2d6edcca8aa632
Chukwuma Obasi <ccobasi8@gmail.com>
2021-06-02 21:09:13 +0100

    Add home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+import initPage from './initial';
+import homePage from './home';
+
+function html() {
+    const link = document.querySelector("link[rel*='icon']")
+      || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+  
+  const content = document.querySelector('.content');
+  content.append(initPage());
+
+  const mainContent = document.querySelector('.main-content');
+  mainContent.append(homePage());
+  mainContent.append(html());
